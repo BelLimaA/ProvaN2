c8ec87477dfe9e7997041a98f00c2b62c496dfa9
BelLimaA <maria.isabel.lima06@aluno.ifce.edu.br>
2021-09-13 17:38:45 -0300

    atividade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  name: String = "";
+  password: String = "";
+
+  ngOnInit(): void {
+    
+  }
+
+  handleClick() {
+    console.log("Nome: " + this.name);
+    console.log("Senha: " + this.password);
+  }
+
   title = 'provan2';
 }
